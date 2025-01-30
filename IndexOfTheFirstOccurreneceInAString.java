da7bb16a33a83078f3076c747d42e00efd7d1600
vishnu3052 <vishnusurevv2003@gmail.com>
2025-01-30 21:43:00 +0530

    Create IndexOfTheFirstOccurreneceInAString.java

---
+++ IndexOfTheFirstOccurreneceInAString.java
@@ -0,0 +1,10 @@
+public class IndexOfTheFirstOccurreneceInAString {
+
+	public static void main(String[] args) {
+		String str="leetcode";
+		String s="leeto";
+		int ind=str.indexOf(s);
+		System.out.println(ind);
+	}
+
+}
